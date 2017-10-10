ee222b31d7d76d3531b2d853c5b62954d4b2699e
GathoMauricio <mauricio2769@gmail.com>
2017-10-09 22:05:56 -0500

    fix fecha catalogo

--- includes/catalogo.php
+++ includes/catalogo.php
@@ -6,7 +6,7 @@
 <th>Nombre</th>
 <th>Fecha de nacimiento</th>
 <th>Edad</th>
-<th>Total de consulta</th>
+<th>Total de consultas</th>
 <th>Estatus</th>
 </tr>
 <?php 
@@ -16,13 +16,21 @@ $sql = "SELECT * FROM paciente ";
 $datos = $con->select($sql);
 while($fila=mysqli_fetch_array($datos))
 {
+$sql = "SELECT count(*) as num FROM consulta WHERE id_paciente=".$fila['id_paciente'];	
+$datos2 = $con->select($sql);
+if($fila2=mysqli_fetch_array($datos2))
+{
+	$numCons=$fila2['num'];
+}
+$fecha=explode('-',$fila['naci_paci']);
+$fecha=$fecha[2].'-'.$fecha[1].'-'.$fecha[0];
 echo'
 <tr style="cursor:pointer;" onclick="abrirExpediente('.$fila['id_paciente'].');">
 <td>'.$fila['id_paciente'].'</td>
 <td>'.$fila['nombre_paci'].' '.$fila['paterno_paci'].' '.$fila['materno_paci'].'</td>
-<td>'.$fila['naci_paci'].'</td>
+<td>'.$fecha.'</td>
 <td>'.$fila['edad_paci'].'</td>
-<td>Total de consulta</td>
+<td>'.$numCons.'</td>
 <td>'.$fila['edo_exp'].'</td>
 </tr>
 ';
